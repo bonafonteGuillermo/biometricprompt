b8d241aaa1c6de06b3c39e5944dee8725ca0092f
Guillermo Bonafonte <guillermo.bonafontecriado@cognizant.com>
2018-11-28 13:38:28 +0100

    Migrated to AndroidX

--- app/src/main/java/app/promtp/biometric/guillermo/example/com/biometricapplication/MainActivity.kt
+++ app/src/main/java/app/promtp/biometric/guillermo/example/com/biometricapplication/MainActivity.kt
@@ -1,6 +1,6 @@
 package app.promtp.biometric.guillermo.example.com.biometricapplication
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
